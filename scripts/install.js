60cc2acf46a930e70a592e1af486b9c55fa7e919
Giovanni Calò <giovanni.calo@live.it>
2021-08-02 01:03:51 +0100

    Added install script

---
+++ scripts/install.js
@@ -0,0 +1,19 @@
+const { promises: { mkdir, rename } } = require("fs");
+const { get } = require("https");
+const { join } = require("path");
+
+const { Extract } = require("unzipper");
+
+get(
+	"https://codeload.github.com/libjpeg-turbo/libjpeg-turbo/zip/refs/tags/2.1.0",
+	async (response) => {
+		const path = join(__dirname, "..", "libraries");
+		await mkdir(path, { recursive: true });
+		response.pipe(Extract({ path })).on("close", async () => {
+			await rename(
+				join(path, "libjpeg-turbo-2.1.0"),
+				join(path, "libjpeg-turbo")
+			);
+		});
+	}
+);
