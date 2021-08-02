c8580fb704606e95c209ff0d9c66dd7b15482932
Giovanni Calò <giovanni.calo@live.it>
2021-08-02 01:08:12 +0100

    Added image module

---
+++ source/native/image.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdint>
+
+#include "format.hpp"
+
+namespace nodeJpeg {
+
+	class Image {
+
+		public:
+
+			Image(
+				Format format,
+				uint32_t width,
+				uint32_t height,
+				uint64_t size
+			);
+
+			~Image();
+
+			uint8_t* data = nullptr;
+
+			Format format;
+
+			uint32_t height = 0;
+
+			uint64_t size = 0;
+
+			uint32_t width = 0;
+
+	};
+
+}
