2df01450acef1537acdef35c1853625da2d329ac
Giovanni Calò <giovanni.calo@live.it>
2021-08-02 04:17:44 +0100

    Added benchmark script

---
+++ scripts/benchmark.js
@@ -0,0 +1,43 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
+
+const { Format, decode, encode } = require("../source");
+
+const iterations = 1000;
+
+const path = join(__dirname, "..", "test");
+
+const jpeg = readFileSync(join(path, "512x512.jpg"));
+
+const rgba = {
+	data: readFileSync(join(path, "512x512.rgba")),
+	format: Format.rgba,
+	height: 512,
+	width: 512
+};
+
+const yuv = {
+	data: readFileSync(join(path, "512x512.yuv")),
+	format: Format.yuv,
+	height: 512,
+	width: 512
+};
+
+const benchmark = async (label, method, ...parameters) => {
+	const start = Date.now();
+	for (let i = 0; i < iterations; i++) {
+		await method(...parameters); // eslint-disable-line no-await-in-loop
+	}
+	console.log(`${label}: ${((Date.now() - start) / iterations).toFixed(2)}ms`);
+};
+
+(async () => {
+	for (const parameters of [
+		["JPEG => RGBA", decode, jpeg, Format.rgba],
+		["JPEG => YUV ", decode, jpeg, Format.yuv],
+		["RGBA => JPEG", encode, rgba, 90],
+		["YUV  => JPEG", encode, yuv, 90]
+	]) {
+		await benchmark(...parameters); // eslint-disable-line no-await-in-loop
+	}
+})();
