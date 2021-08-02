aa93f17ece252d584f7dd0e5d2cb43cb07318380
Giovanni Calò <giovanni.calo@live.it>
2021-08-02 01:09:08 +0100

    Added decoder module

---
+++ source/native/decoder.cpp
@@ -0,0 +1,120 @@
+#include "decoder.hpp"
+
+namespace nodeJpeg {
+
+	Decoder::Decoder(
+		Napi::Env environment,
+		Napi::Uint8Array source,
+		Format format
+	) :
+		Napi::AsyncWorker(environment),
+		format(format),
+		promise(Napi::Promise::Deferred::New(environment)),
+		source(source)
+	{}
+
+	void Decoder::Execute() {
+		tjhandle handle = tjInitDecompress();
+		if (!handle) {
+			SetError("[nodeJpeg::Decoder::Execute] Failed to initialize");
+			return;
+		}
+		int32_t colorspace = 0;
+		int32_t height = 0;
+		int32_t subsampling = 0;
+		int32_t width = 0;
+		if (tjDecompressHeader3(
+			handle,
+			source.Data(),
+			source.ByteLength(),
+			&width,
+			&height,
+			&subsampling,
+			&colorspace
+		)) {
+			SetError("[nodeJpeg::Decoder::Execute] Failed to read header");
+			tjDestroy(handle);
+			return;
+		};
+		if (format == Format::yuv) {
+			// TODO: Support other colorspaces
+			if (colorspace != TJCS_YCbCr) {
+				SetError("[nodeJpeg::Decoder::Execute] Input image colorspace is not YUV");
+				tjDestroy(handle);
+				return;
+			}
+			// TODO: Support other subsamplings
+			if (subsampling != TJSAMP_420) {
+				SetError("[nodeJpeg::Decoder::Execute] Input image subsampling is not 4:2:0");
+				tjDestroy(handle);
+				return;
+			}
+			// TODO: Support odd dimensions
+			height = (height + 1) & ~1;
+			width = (width + 1) & ~1;
+			uint32_t size = tjBufSizeYUV2(width, 1, height, TJSAMP_420);
+			image = new Image(Format::yuv, width, height, size);
+			std::memset(image->data, 0, size);
+			if (tjDecompressToYUV2(
+				handle,
+				source.Data(),
+				source.ByteLength(),
+				image->data,
+				width,
+				1,
+				height,
+				0
+			)) {
+				SetError("[nodeJpeg::Decoder::Execute] Failed to decode");
+				delete image;
+			}
+		} else if (format == Format::rgba) {
+			// TODO: Support RGBA
+			SetError("[nodeJpeg::Decoder::Execute] Decoding to RGBA is not yet supported");
+		} else {
+			SetError("[nodeJpeg::Decoder::Execute] Format is invalid");
+		}
+		tjDestroy(handle);
+	}
+
+	void Decoder::OnError(const Napi::Error& error) {
+		promise.Reject(error.Value());
+	}
+
+	void Decoder::OnOK() {
+		Napi::Object output = Napi::Object::New(Env());
+		output.Set("data", Napi::Uint8Array::New(
+			Env(),
+			image->size,
+			Napi::ArrayBuffer::New(
+				Env(),
+				image->data,
+				image->size,
+				[](Napi::Env, void*, void* image) {
+					delete static_cast<Image*>(image);
+				},
+				image
+			),
+			0,
+			napi_uint8_clamped_array
+		));
+		output.Set(
+			"format",
+			Napi::Number::New(Env(), image->format)
+		);
+		output.Set(
+			"height",
+			Napi::Number::New(Env(), image->height)
+		);
+		output.Set(
+			"width",
+			Napi::Number::New(Env(), image->width)
+		);
+		promise.Resolve(output);
+	}
+
+	Napi::Promise Decoder::getPromise() {
+		return promise.Promise();
+	}
+
+}
---
+++ source/native/decoder.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cmath>
+
+#include <napi.h>
+
+extern "C" {
+	#include <turbojpeg.h>
+}
+
+#include "format.hpp"
+#include "image.hpp"
+
+namespace nodeJpeg {
+
+	class Decoder : public Napi::AsyncWorker {
+
+		private:
+
+			Format format;
+
+			Image* image = nullptr;
+
+			Napi::Promise::Deferred promise;
+
+			Napi::Uint8Array source;
+
+		public:
+
+			Decoder(
+				Napi::Env environment,
+				Napi::Uint8Array source,
+				Format format
+			);
+
+			~Decoder() override = default;
+
+			void Execute() override;
+
+			void OnError(const Napi::Error& error) override;
+
+			void OnOK() override;
+
+			Napi::Promise getPromise();
+
+	};
+
+}
