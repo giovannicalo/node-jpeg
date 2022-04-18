81dc9fd17124b512924d7a30edc1bfc8a657caf7
Giovanni Calò <giovanni.calo@live.it>
2022-04-18 16:18:04 +0100

    Updated native dependencies

--- scripts/install.js
+++ scripts/install.js
@@ -5,13 +5,13 @@ const { join } = require("path");
 const { Extract } = require("unzipper");
 
 get(
-	"https://codeload.github.com/libjpeg-turbo/libjpeg-turbo/zip/refs/tags/2.1.0",
+	"https://codeload.github.com/libjpeg-turbo/libjpeg-turbo/zip/refs/tags/2.1.3",
 	async (response) => {
 		const path = join(__dirname, "..", "libraries");
 		await mkdir(path, { recursive: true });
 		response.pipe(Extract({ path })).on("close", async () => {
 			await rename(
-				join(path, "libjpeg-turbo-2.1.0"),
+				join(path, "libjpeg-turbo-2.1.3"),
 				join(path, "libjpeg-turbo")
 			);
 		});
