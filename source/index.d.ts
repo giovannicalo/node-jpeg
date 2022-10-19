2b89220c48c1f68a6ac06db3424a5e9a484c86ad
Giovanni Calò <giovanni.calo@live.it>
2022-10-19 03:42:38 +0100

    Improved types

--- source/index.d.ts
+++ source/index.d.ts
@@ -22,7 +22,7 @@ declare namespace jpeg {
 	 * @param {Format} [format=Format.rgba] - The desired output format, either RGBA or YUV I420, defaults to RGBA.
 	 * @returns {Promise<Image>} A promise resolving to the decoded image.
 	 */
-	function decode(data: Buffer, format: Format): Promise<Image>;
+	function decode(data: Buffer, format?: Format): Promise<Image>;
 
 	/**
 	 * Encodes a raw image as JPEG.
@@ -31,7 +31,7 @@ declare namespace jpeg {
 	 * @param {Number} [quality=90] - The desired output quality, between 0 and 100, defaults to 90.
 	 * @returns {Promise<Buffer>} A promise resolving to the encoded buffer.
 	 */
-	function encode(image: Image, quality: number): Promise<Buffer>;
+	function encode(image: Image, quality?: number): Promise<Buffer>;
 
 }
 
